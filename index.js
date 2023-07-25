a5691783d62f3cdc9cc579b2406da0dc379ce17d
Jacob <jakelausier7878@gmail.com>
2023-07-24 20:38:39 -0400

    fixed file structure working on errors

--- index.js
+++ index.js
@@ -1,69 +1,60 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const { Circle, Triangle, Square } = require('./lib/shapes');
 
-//function to generate the SVG content based on user input
-function generateSVG(color, shape, text) {
-    let svgContent;
-    switch (shape){
-        case "circle":
-            svgContent = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="50" cy="50" r="50" style="fill:${color};"/>
-            <text x="10" y="50" fill="white">${text}</text>
-          </svg>`;
+async function generateLogo() {
+  try {
+    const userInput = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal number):',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal number):',
+      },
+    ]);
+
+    let shape;
+    switch (userInput.shape) {
+      case 'circle':
+        shape = new Circle(userInput.shapeColor);
         break;
-        case "square":
-            svgContent = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-              <rect width="100" height="100" style="fill:${color};"/>
-              <text x="10" y="50" fill="white">${text}</text>
-            </svg>`;
+      case 'triangle':
+        shape = new Triangle(userInput.shapeColor);
         break;
-         case "triangle":
-                svgContent = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-                  <polygon points="50,0 0,100 100,100" style="fill:${color};"/>
-                  <text x="10" y="50" fill="white">${text}</text>
-                </svg>`;
-         break;
-         default:
-      svgContent = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100" height="100" style="fill:${color};"/>
-        <text x="10" y="50" fill="white">${text}</text>
-      </svg>`;
+      case 'square':
+        shape = new Square(userInput.shapeColor);
+        break;
     }
-  return svgContent;
+
+    const svgCode = shape.generateSVG();
+
+    // Save the SVG code to a file named `logo.svg`
+    const outputFilePath = path.join(__dirname, 'logo.svg');
+    fs.writeFileSync(outputFilePath, svgCode);
+    // Write the SVG code to `logo.svg`
+
+    console.log('Generated logo.svg');
+  } catch (error) {
+    console.error('Error generating logo:', error);
+  }
 }
 
-//function to save the generated SVG to a file
-function saveSVGTofile(svgContent) {
-  const filename = "logo.svg";
-  fs.writeFile(filename, svgContent, (err) => {
-    if (err) throw err;
-    console.log(`Logo Saved to ${filename}`);
-  });
-}
+generateLogo();
 
-// Prompt user for input
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "text",
-      message: "Enter the text for your logo",
-    },
-    {
-      type: "list",
-      name: "color",
-      message: 'Select a color',
-      choices: ["red", "green", "blue", "yellow", "orange", "purple"],
-    },
-    {
-      type: "list",
-      name: "shape",
-      message: "Select a shape",
-      choices: ["circle", "rect", "square", "triangle"],
-    },
-  ])
-  .then((answers) => {
-    const { color, shape, text } = answers;
-    const svgContent = generateSVG(color, shape, text);
-    saveSVGTofile(svgContent);
-  });
+
+
+
